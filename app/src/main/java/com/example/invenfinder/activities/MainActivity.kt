1ab3d31d69e43a55a67cd6c3510491de3c26da48
Mikhail Stefantsev <stephan05.08.67@gmail.com>
2022-09-15 15:11:29 +0300

    added filtering

--- app/src/main/java/com/example/invenfinder/activities/MainActivity.kt
+++ app/src/main/java/com/example/invenfinder/activities/MainActivity.kt
@@ -4,13 +4,15 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.setValue
+import androidx.compose.material.TextField
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import com.example.invenfinder.components.TitleBar
 import com.example.invenfinder.data.Item
 import com.example.invenfinder.utils.ItemManager
 import com.example.invenfinder.utils.Preferences
@@ -19,7 +21,8 @@ import kotlinx.coroutines.launch
 
 
 class MainActivity : ComponentActivity() {
-	private var items by mutableStateOf(listOf<Item>())
+	private var items by mutableStateOf(mutableListOf<Item>())
+	var filteredItems by mutableStateOf(mutableListOf<Item>())
 
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
@@ -28,7 +31,12 @@ class MainActivity : ComponentActivity() {
 		Preferences.setPreferences(prefs)
 
 		setContent {
-			ItemList(items)
+			var searchQuery by remember { mutableStateOf("") }
+			Column {
+				TitleBar("Inventory")
+				SearchField(searchQuery, onQueryChange = { q -> searchQuery = q; filteredItems = filter(items, q) })
+				ItemList(filteredItems)
+			}
 		}
 	}
 
@@ -36,19 +44,7 @@ class MainActivity : ComponentActivity() {
 		super.onResume()
 		MainScope().launch {
 			items = loadItems()
-		}
-	}
-
-	@Composable
-	fun ItemList(items: List<Item>) {
-		if (items.isEmpty()) {
-			Text("No items")
-		} else {
-			LazyColumn {
-				items(items) { item ->
-					Text(item.name)
-				}
-			}
+			filteredItems = items
 		}
 	}
 
@@ -61,4 +57,44 @@ class MainActivity : ComponentActivity() {
 			ArrayList()
 		}
 	}
+}
+
+@Composable
+fun ItemList(items: List<Item>) {
+	if (items.isEmpty()) {
+		Text("No items")
+	} else {
+		LazyColumn {
+			items(items) { item ->
+				Text(item.name)
+			}
+		}
+	}
+}
+
+@Composable
+fun SearchField(query: String = "", onQueryChange: (String) -> Unit = {}) {
+	TextField(value = query, onValueChange = onQueryChange, modifier = Modifier.fillMaxWidth())
+}
+
+fun filter(items: MutableList<Item>, query: String?): MutableList<Item> {
+	val filtered = ArrayList<Item>()
+
+	if (query == null || query.isEmpty()) {
+		filtered.addAll(items)
+	} else {
+		val q = query.trim().lowercase()
+		val l = query.trim().lowercase()
+
+		for (c in items) {
+			if (c.name.lowercase().contains(q)
+				|| c.description!!.lowercase().contains(q)
+				|| c.location == l.lowercase()
+			) {
+				filtered.add(c)
+			}
+		}
+	}
+
+	return filtered
 }
